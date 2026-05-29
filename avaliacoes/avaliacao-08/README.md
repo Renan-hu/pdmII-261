9028c5c9f61639baaf684f66bf3c60273c22df6b
Renan <santos.sousa11@aluno.ifce.edu.br>
2026-05-28 22:22:59 -0300

    Atividade 8

---
+++ avaliacoes/avaliacao-08/README.md
@@ -0,0 +1,1 @@
+![alt text](github_Client.png)
---
+++ avaliacoes/avaliacao-08/github_client/lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = 'Ov23li0UYzmnlFXfDI5X';
+const githubClientSecret = '368e8dff511cdf1236f8a3a2bf990bf08c559015';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
